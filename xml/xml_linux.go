8dd2de096ea812f2c64fbc58f5c936bfb9d2db0e
sqjian <shengqi.jian@qq.com>
2021-04-10 17:27:27 +0800

    add test data

--- xml/xml_linux.go
+++ xml/xml_linux.go
@@ -1,50 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"github.com/lestrrat-go/libxml2"
 	"github.com/lestrrat-go/libxml2/xsd"
-	"io/ioutil"
-	"log"
-	"os"
 )
 
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
+func ValidateXmlWithXsd(xmlData, xsdData []byte) error {
+	xsdInstance, xsdInstanceErr := xsd.Parse(xsdData)
+	if xsdInstanceErr != nil {
+		return xsdInstanceErr
 	}
-}
+	defer xsdInstance.Free()
 
-func main() {
-	xsdFile := "test.xsd"
-	f, err := os.Open(xsdFile)
-	check(err)
-	defer f.Close()
-
-	buf, err := ioutil.ReadAll(f)
-	check(err)
-
-	s, err := xsd.Parse(buf)
-	check(err)
-	defer s.Free()
-
-	xmlFile := "test.xml"
-	f2, err := os.Open(xmlFile)
-	check(err)
-	defer f2.Close()
-
-	buf2, err := ioutil.ReadAll(f2)
-	check(err)
-
-	d, err := libxml2.Parse(buf2)
-	check(err)
-
-	if err := s.Validate(d); err != nil {
-		for _, e := range err.(xsd.SchemaValidationError).Errors() {
-			log.Printf("error: %s", e.Error())
-		}
-		return
+	xmlInstance, xmlInstanceErr := libxml2.Parse(xmlData)
+	if xmlInstanceErr != nil {
+		return xmlInstanceErr
 	}
+	defer xmlInstance.Free()
 
-	fmt.Println("Validation of", xmlFile, "against", xsdFile, "successful!")
+	if ValidateErr := xsdInstance.Validate(xmlInstance); ValidateErr != nil {
+		return ValidateErr
+	}
 }
