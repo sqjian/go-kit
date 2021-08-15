003d04ecf62a1805338c1abe3b8395683877cc31
sqjian <shengqi.jian@qq.com>
2021-08-15 09:56:36 +0800

    sync

--- pool/connection/opt.go
+++ pool/connection/opt.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-type option interface {
+type Option interface {
 	apply(*ClientPool)
 }
 
@@ -16,73 +16,73 @@ func (f OptionFunc) apply(options *ClientPool) {
 	f(options)
 }
 
-func WithAddress(Address string) option {
+func WithAddress(Address string) Option {
 	return OptionFunc(func(options *ClientPool) {
 		options.Address = Address
 	})
 }
 
-func WithPort(Port string) option {
+func WithPort(Port string) Option {
 	return OptionFunc(func(options *ClientPool) {
 		options.Port = Port
 	})
 }
 
-func WithDial(Dial func(ctx context.Context, address, port string) (connection interface{}, err error)) option {
+func WithDial(Dial func(ctx context.Context, address, port string) (connection interface{}, err error)) Option {
 	return OptionFunc(func(options *ClientPool) {
 		options.Dial = Dial
 	})
 }
 
-func WithClose(Close func(ctx context.Context, connection interface{}) (err error)) option {
+func WithClose(Close func(ctx context.Context, connection interface{}) (err error)) Option {
 	return OptionFunc(func(options *ClientPool) {
 		options.Close = Close
 	})
 }
 
-func WithKeepAlive(KeepAlive func(ctx context.Context, connection interface{}) (err error)) option {
+func WithKeepAlive(KeepAlive func(ctx context.Context, connection interface{}) (err error)) Option {
 	return OptionFunc(func(options *ClientPool) {
 		options.KeepAlive = KeepAlive
 	})
 }
 
-func WithInitialPoolSize(InitialPoolSize int) option {
+func WithInitialPoolSize(InitialPoolSize int) Option {
 	return OptionFunc(func(options *ClientPool) {
 		options.InitialPoolSize = InitialPoolSize
 	})
 }
 
-func WithMaxPoolSize(MaxPoolSize int) option {
+func WithMaxPoolSize(MaxPoolSize int) Option {
 	return OptionFunc(func(options *ClientPool) {
 		options.MaxPoolSize = MaxPoolSize
 	})
 }
 
-func WithDialRetryCount(DialRetryCount int) option {
+func WithDialRetryCount(DialRetryCount int) Option {
 	return OptionFunc(func(options *ClientPool) {
 		options.DialRetryCount = DialRetryCount
 	})
 }
 
-func WithKeepAliveInterval(KeepAliveInterval time.Duration) option {
+func WithKeepAliveInterval(KeepAliveInterval time.Duration) Option {
 	return OptionFunc(func(options *ClientPool) {
 		options.KeepAliveInterval = KeepAliveInterval
 	})
 }
 
-func WithDialRetryInterval(DialRetryInterval time.Duration) option {
+func WithDialRetryInterval(DialRetryInterval time.Duration) Option {
 	return OptionFunc(func(options *ClientPool) {
 		options.DialRetryInterval = DialRetryInterval
 	})
 }
 
-func WithCreateNewInterval(CreateNewInterval time.Duration) option {
+func WithCreateNewInterval(CreateNewInterval time.Duration) Option {
 	return OptionFunc(func(options *ClientPool) {
 		options.CreateNewInterval = CreateNewInterval
 	})
 }
 
-func WithLogger(Logger log.Logger) option {
+func WithLogger(Logger log.Logger) Option {
 	return OptionFunc(func(options *ClientPool) {
 		options.Logger = Logger
 	})
