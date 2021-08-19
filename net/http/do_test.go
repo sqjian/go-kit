e4a7d50f3dbb4adda4b3d4d6f80e9b305d27f6fc
sqjian <shengqi.jian@qq.com>
2021-08-19 09:48:13 +0800

    sync

--- net/http/do_test.go
+++ net/http/do_test.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	"fmt"
 	httpUtil "github.com/sqjian/go-kit/net/http"
-	"log"
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"testing"
 	"time"
 )
@@ -43,23 +41,16 @@ func TestDo(t *testing.T) {
 	t.Logf("rst:%v,err:%v", string(rst), err)
 }
 
-const (
-	testPort = 8888
-)
-
-func TestServer(t *testing.T) {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		_, _ = fmt.Fprintf(writer, "Hello, there\n")
-	})
-	fmt.Println("Server started at port " + strconv.Itoa(testPort))
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(testPort), nil))
-}
-
 func BenchmarkDo(b *testing.B) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(500 * 1e6)
+		fmt.Fprintln(w, "hello")
+	}))
+
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
 			_, err := httpUtil.Do(
-				"http://172.31.243.215:"+strconv.Itoa(testPort),
+				ts.URL,
 				httpUtil.GET,
 				map[string]string{
 					"from":    "cn",
