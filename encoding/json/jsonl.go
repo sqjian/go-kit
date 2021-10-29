71ae9f1b59b59c58bd484bb2c5b0ffccde07763b
sqjian <shengqi.jian@qq.com>
2021-10-29 17:17:56 +0800

    sync

--- encoding/json/jsonl.go
+++ encoding/json/jsonl.go
@@ -22,8 +22,6 @@ func getOriginalSlice(ptrToSlice interface{}) (slice reflect.Value, err error) {
 	return originalSlice, nil
 }
 
-// Decode reads the next JSON Lines-encoded value that reads
-// from r and stores it in the slice pointed to by ptrToSlice.
 func Decode(r io.Reader, ptrToSlice interface{}) error {
 	originalSlice, err := getOriginalSlice(ptrToSlice)
 	if err != nil {
@@ -33,7 +31,6 @@ func Decode(r io.Reader, ptrToSlice interface{}) error {
 	slElem := originalSlice.Type().Elem()
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
-		//create new object
 		newObj := reflect.New(slElem).Interface()
 		item := scanner.Bytes()
 		err := json.Unmarshal(item, newObj)
@@ -49,7 +46,6 @@ func Decode(r io.Reader, ptrToSlice interface{}) error {
 	return nil
 }
 
-// Encode writes the JSON Lines encoding of ptrToSlice to the w stream
 func Encode(w io.Writer, ptrToSlice interface{}) error {
 	originalSlice, err := getOriginalSlice(ptrToSlice)
 	if err != nil {
