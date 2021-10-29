ae8104cb6738b472dcecd2e9dc3e25b050ea0e20
sqjian <shengqi.jian@qq.com>
2021-10-29 17:17:56 +0800

    sync

--- encoding/json/jsonl_test.go
+++ encoding/json/jsonl_test.go
@@ -15,16 +15,16 @@ type Person struct {
 }
 
 func TestDecode(t *testing.T) {
-	people := []Person{}
+	var people []Person
 	err := Decode(strings.NewReader(data), &people)
 	if err != nil {
-		t.Fatalf("Decode returns error: ", err)
+		t.Fatal("Decode returns error: ", err)
 	}
 	if len(people) != 2 {
-		t.Fatalf("Expected 2 objects in slice, got %s", len(people))
+		t.Fatalf("Expected 2 objects in slice, got %v", len(people))
 	}
 	if people[0].Name != "Paul" {
-		t.Fatalf("Unexpected value in first object Name field: ", people[0].Name)
+		t.Fatalf("Unexpected value in first object Name field: %v", people[0].Name)
 	}
 }
 
@@ -32,10 +32,10 @@ func TestDecodeWrongTypes(t *testing.T) {
 	people := map[Person]int{}
 	err := Decode(strings.NewReader(data), &people)
 	if err == nil {
-		t.Fatal("Decode doesnt returns error")
+		t.Fatal("Decode doesn't returns error")
 	}
 	if err.Error() != "expected pointer to slice, got pointer to map" {
-		t.Fatalf("Decode return wrong error: ", err)
+		t.Fatalf("Decode return wrong error: %v", err)
 	}
 }
 
@@ -51,7 +51,6 @@ func TestEncode(t *testing.T) {
 		t.Fatal("Encode returns error: ", err)
 	}
 	if !strings.Contains(buf.String(), data) {
-		t.Fatalf("Encode return wrong data, expected: %s, got: %s",
-			data, buf.String())
+		t.Fatalf("Encode return wrong data, expected: %s, got: %s", data, buf.String())
 	}
 }
