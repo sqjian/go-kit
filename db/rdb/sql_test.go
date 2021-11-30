6a1778d87464dcc297168177f744ebc1bd4c73cd
sqjian <shengqi.jian@qq.com>
2021-11-30 14:50:02 +0800

    release 33

--- db/rdb/sql_test.go
+++ db/rdb/sql_test.go
@@ -52,6 +52,27 @@ func Test_Insert(t *testing.T) {
 	t.Log(rdb.Insert(ctx, "test", map[string]interface{}{"column_1": 1}))
 }
 
+func Test_BatchInsert(t *testing.T) {
+	checkErr := func(err error) {
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	rdb, rdbErr := rdb.NewRdb(
+		rdb.Sqlite,
+		rdb.WithMaxLifeTime(time.Second),
+		rdb.WithMaxIdleConns(10),
+		rdb.WithDbName("sqlite"),
+		rdb.WithLogger(log.DebugLogger),
+	)
+	checkErr(rdbErr)
+
+	ctx := context.WithValue(context.Background(), "id", 1)
+
+	t.Log(rdb.Insert(ctx, "test", map[string]interface{}{"column_1": 1}))
+}
+
 func Test_Update(t *testing.T) {
 	checkErr := func(err error) {
 		if err != nil {
