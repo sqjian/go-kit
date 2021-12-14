017aa3d414924aa17e3a51703f742e224cf16809
sqjian <shengqi.jian@qq.com>
2021-12-14 14:23:06 +0800

    41

---
+++ db/rdb/helper.go
@@ -0,0 +1,18 @@
+package rdb
+
+/*
+meta 元数据信息：
+idc：调用 idc 机房名称 hf、bj、gz
+schema：调用数据库名
+table：调用表名
+param 查询参数：
+countJson：getCount 接口用于统计的字段
+columnJson：getList 接口用于获取的字段列表
+whereJson：查询条件
+groupJson：分组条件
+havingJson：分组过滤条件
+orderJson：排序条件
+limitJson：分页条件
+insertJson：插入数据
+setJson：更新数据
+*/
