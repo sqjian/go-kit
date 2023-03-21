1e5b115bc1606e75b41e62aea66193f9cb22dce0
sqjian <shengqi.jian@qq.com>
2023-03-21 08:43:18 +0800

    add log global fn

--- log/preset.go
+++ log/preset.go
@@ -14,18 +14,27 @@ func (DummyLogger) Errorf(string, ...interface{}) {}
 
 type TerminalLogger struct{}
 
+func Debugf(template string, args ...interface{}) {
+	(&TerminalLogger{}).Debugf(template, args...)
+}
 func (t TerminalLogger) Debugf(template string, args ...interface{}) {
 	_, _ = fmt.Fprintf(os.Stderr, template, args...)
 }
-
+func Infof(template string, args ...interface{}) {
+	(&TerminalLogger{}).Debugf(template, args...)
+}
 func (t TerminalLogger) Infof(template string, args ...interface{}) {
 	_, _ = fmt.Fprintf(os.Stderr, template, args...)
 }
-
+func Warnf(template string, args ...interface{}) {
+	(&TerminalLogger{}).Debugf(template, args...)
+}
 func (t TerminalLogger) Warnf(template string, args ...interface{}) {
 	_, _ = fmt.Fprintf(os.Stderr, template, args...)
 }
-
+func Errorf(template string, args ...interface{}) {
+	(&TerminalLogger{}).Debugf(template, args...)
+}
 func (t TerminalLogger) Errorf(template string, args ...interface{}) {
 	_, _ = fmt.Fprintf(os.Stderr, template, args...)
 }
--- upTag.bat
+++ upTag.bat
@@ -1,4 +1,4 @@
-set tag=v0.0.46
+set tag=v0.0.47
 
 git tag -d %tag%
 git push origin :refs/tags/%tag%
