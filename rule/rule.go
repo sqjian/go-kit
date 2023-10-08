5ecb59a8cb0a293a87e0ccff2fbc12232c57546e
sqjian <shengqi.jian@qq.com>
2023-10-08 08:54:04 +0800

    update

--- rule/rule.go
+++ rule/rule.go
@@ -8,13 +8,21 @@ import (
 
 var buildCache sync.Map
 
+func Compile(code string) (*vm.Program, error) {
+	return expr.Compile(code)
+}
+
+func Run(program *vm.Program, env any) (any, error) {
+	return expr.Run(program, env)
+}
+
 func ExecRule(code string, env any) (any, error) {
 	program, _ := buildCache.LoadOrStore(code, func() *vm.Program {
-		_program, compileErr := expr.Compile(code)
+		_program, compileErr := Compile(code)
 		if compileErr != nil {
 			panic(compileErr)
 		}
 		return _program
 	}())
-	return expr.Run(program.(*vm.Program), env)
+	return Run(program.(*vm.Program), env)
 }
